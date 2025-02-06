0674ae9496de779bc0504e2d36d0b57e40087620
Vasile Eftodii <veftodii@users.noreply.github.com>
2025-02-06 15:53:56 +0000

    Add lesson 3

---
+++ lesson3.py
@@ -0,0 +1,4 @@
+name = input() # brackets necessary
+print(name, 'hgjh') # default value separator is space
+print(name + 'kldf') # concatenates the strings
+print(name * 3) # repeats the string N times
