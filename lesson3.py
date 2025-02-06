4696ddb745d32cf4ed00e73794031e38c129daf4
Vasile Eftodii <veftodii@users.noreply.github.com>
2025-02-06 17:07:08 +0000

    Test strings and numbers

--- lesson3.py
+++ lesson3.py
@@ -1,4 +1,13 @@
+print('Type a string:')
 name = input() # brackets necessary
-print(name, 'hgjh') # default value separator is space
-print(name + 'kldf') # concatenates the strings
-print(name * 3) # repeats the string N times
+print('Value separator:', name, 'hgjh') # default value separator is space
+print('Concatenate string: ' + name + 'kldf') # concatenates the strings
+print('Repeat string:', name * 3) # repeats the string N times
+print('String length:', len(name)) # string length
+
+print('\nType a number:')
+age = input() # returns a string
+print('Number:', age)
+print('Number to string:', str(age))
+print('Number add string, to string:', str(age + '1'))
+print('Number add integer, to string:', str(int(age) + 1)) # variable is a string
