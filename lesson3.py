4c07e2ba1c71e3612a21cd5f1e05c79585fbc20f
Vasile Eftodii <veftodii@users.noreply.github.com>
2025-02-07 15:09:00 +0000

    Add bool expressions

--- lesson3.py
+++ lesson3.py
@@ -1,5 +1,6 @@
 print('Type a string:')
 name = input() # brackets necessary
+print('String:', name)
 print('Value separator:', name, 'hgjh') # default value separator is space
 print('Concatenate string: ' + name + 'kldf') # concatenates the strings
 print('Repeat string:', name * 3) # repeats the string N times
@@ -11,3 +12,13 @@ print('Number:', age)
 print('Number to string:', str(age))
 print('Number add string, to string:', str(age + '1'))
 print('Number add integer, to string:', str(int(age) + 1)) # variable is a string
+print('Number to float:', float(age))
+
+print('\nBool expressions:')
+b = True # Python is case-sensitive
+print('1 = 2:', 1 == 2)
+print('1 = 1:', 1 == 1)
+print('1 != 2:', 1 != 2)
+print('1 != 1:', 1 != 1)
+print('Int and float 1 = 1.0:', 1 == 1.0)
+print('Int and string 1 = \'1\':', 1 == '1') # no implicit cast observed
